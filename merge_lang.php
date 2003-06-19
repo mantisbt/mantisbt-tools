971b02d199e2539ff04da2be1d8c10003b0b0fc8
prescience <prescience@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2003-06-19 17:05:11 +0000

    merge changes from new and old localization files
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2175 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- merge_lang.php
+++ merge_lang.php
@@ -1,20 +1,13 @@
 <?php
 	# -- GLOBAL VARIABLES --
-	$lang_files = array();
-	$english_strings = array();
+	$new_strings = array();
+	$old_strings = array();
 	# - ---
-	# read in all language files
-	function grab_lang_files() {
-		global $lang_files;
+	function load_files( $p_old_file, $p_new_file ) {
+		global $new_strings, $old_strings;
 
-		if ($handle = opendir('.')) {
-		    while (false !== ($file = readdir($handle))) {
-			    if (strpos($file,'.txt')>0) {
-			    	$lang_files[] = $file;
-			    }
-		    }
-		    closedir($handle);
-		}
+		$new_strings = file( $p_new_file );
+		$old_strings = file( $p_old_file );
 	}
 	# - ---
 	function found( $p_haystack, $p_needle ) {
@@ -36,19 +29,85 @@
 		return $p_array[0];
 	}
 	# - ---
-	function check_missing( $p_file ) {
-		$strings = file( $p_file );
-		$lang_strings = array();
+	# parse the string and grab the variable
+	function get_data( $p_string ) {
+		$p_string = trim( $p_string );
+		if ( '$' != $p_string[0] ) {
+			return '';
+		}
+		$p_string = str_replace( ' ', '', $p_string );
+		$p_array = explode('=', $p_string);
+		return $p_array[1];
+	}
+	# - ---
+	function compare_files() {
+		global $new_strings, $old_strings;
+
+		# create keyed array of the new strings
+		$strings = $new_strings;
+		$new_strings = array();
 		foreach( $strings as $string ) {
 			$string = trim( $string );
 			$key = get_key( $string );
 			if ( strlen( $key ) > 0 ) {
-				$lang_strings[$key] = $string;
+				$new_strings[$key] = $string;
 			}
 		}
-		$fp = fopen( $p_file, 'wb' );
 
+		# loop over original strings
+		# report if new strings are different, same, or missing.
+		$skip = 0;
+		$counter = 0;
+		$diff_counter = 0;
+		$missing_counter = 0;
+		foreach ( $old_strings as $original_string ) {
+			$original_string = trim( $original_string );
+			$p_original_string = trim( $original_string );
+			$counter++;
+			$t_counter = str_pad( $counter, 3, ' ', STR_PAD_LEFT );
+			# ignore header section
+			if ( 0 == $skip ) {
+				if ( '?>' == $p_original_string ) {
+					$skip = 1;
+				}
+			} else {
+				# grab the key
+				$key = get_key( $original_string );
+				if ( empty( $key ) ) {
+					continue;
+				}
+				if ( array_key_exists( $key, $new_strings ) ) {
+					if ( $new_strings[$key] != $original_string ) {
+						$diff_counter++;
+						echo "[$t_counter] [O] $original_string\n";
+						echo "      [N] ".$new_strings[$key]."\n\n";
+					}
+				} else {
+					$missing_counter++;
+					echo "[$t_counter] NEWFILE IS MISSING: $original_string \n\n";
+				}
+			}
+		}
+		echo "Different: $diff_counter\t\tMissing: $missing_counter\n";
+	}
+	# - ---
+	function merge_files( $p_old_file ) {
+		global $new_strings, $old_strings;
+
+		# create keyed array of the new strings
+		$strings = $new_strings;
+		$new_strings = array();
+		foreach( $strings as $string ) {
+			$string = trim( $string );
+			$key = get_key( $string );
+			if ( strlen( $key ) > 0 ) {
+				$new_strings[$key] = $string;
+			}
+		}
+
+		$fp = fopen( $p_old_file, 'wb' );
 		# print out header
+		$strings = $old_strings;
 		foreach( $strings as $string ) {
 			$p_string = trim( $string );
 			if ( '?>' === $p_string ) {
@@ -58,66 +117,67 @@
 			fwrite( $fp, rtrim($string)."\n" );
 		}
 
-		# grab english strings
-		$english_strings = file( 'strings_english.txt' );
+		# loop over original strings
+		# report if new strings are different, same, or missing.
 		$skip = 0;
-		foreach ( $english_strings as $english_string ) {
-			$english_string = trim( $english_string );
-			$p_english_string = trim( $english_string );
+		$diff_counter = 0;
+		$missing_counter = 0;
+		foreach ( $old_strings as $original_string ) {
+			$original_string = trim( $original_string );
+			$p_original_string = trim( $original_string );
 			# ignore header section
 			if ( 0 == $skip ) {
-				if ( '?>' == $p_english_string ) {
+				if ( '?>' == $p_original_string ) {
 					$skip = 1;
 				}
 			} else {
 				# grab the key
-				# if found then use the lang string
-				$key = get_key( $english_string );
-				if ( array_key_exists( $key, $lang_strings ) ) {
-					fwrite( $fp, $lang_strings[$key]."\n" );
-				# else use the english string
-				} else {
-					if ( '?>' == $p_english_string ) {
-						fwrite( $fp, $english_string );
+				$key = get_key( $original_string );
+				if ( empty( $key ) ) {
+					fwrite( $fp, $p_original_string."\n" );
+					continue;
+				}
+				if ( array_key_exists( $key, $new_strings ) ) {
+					if ( $new_strings[$key] != $original_string ) {
+						# diff
+						$diff_counter++;
+						fwrite( $fp, $new_strings[$key]."\n" );
 					} else {
-						fwrite( $fp, $english_string."\n" );
+						fwrite( $fp, $p_original_string."\n" );
 					}
+				} else {
+					$missing_counter++;
+					fwrite( $fp, $p_original_string."\n" );
 				}
 			}
 		}
 		fclose( $fp );
+		echo "Different: $diff_counter\t\tMissing from Newfile: $missing_counter\n";
+		echo "Merged\n";
 	}
 	# - ---
-	function check_duplicates( $p_file ) {
-		$strings = file( $p_file );
-		$lang_strings = array();
-		$counter = 0;
-		foreach( $strings as $string ) {
-			$string = trim( $string );
-			$key = get_key( $string );
-			$counter++;
-			if ( strlen( $key ) > 0 ) {
-				if ( array_key_exists( $key, $lang_strings ) ) {
-					echo "DUPLICATE: $key (line: $counter)\n";
-				} else {
-					$lang_strings[$key] = $string;
-				}
-			}
-		}
+	function print_usage() {
+		echo "\nUsage:\n        php -q merge_lang.php <option> <oldfile> <newfile>\n        -c compare only\n        -m merge\n";
 	}
+	# - ---
 
 	# -- MAIN --
-	print "Checking for missing and duplicate strings.\n";
+	$argv = $_SERVER['argv'];
+	$argc = $_SERVER['argc'];
 
-	grab_lang_files();
+	# too few arguments?
+	if ( $argc < 4 ) {
+		print_usage();
+		exit;
+	}
 
-	#check_lineterms( 'asd.txt' );
-
-	foreach( $lang_files as $file ) {
-		if ( 'strings_english.txt' != $file ) {
-			echo "Processing: $file\n";
-			check_missing( $file );
-			check_duplicates( $file );
-		}
+	if ( '-c' == $argv[1] ) {
+		load_files( $argv[2], $argv[3] );
+		compare_files();
+	} else if ( '-m' == $argv[1] ) {
+		load_files( $argv[2], $argv[3] );
+		merge_files( $argv[2] );
+	} else {
+		print_usage();
 	}
 ?>

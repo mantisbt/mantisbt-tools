25b0d06bb59c3183bc8b808eb01020c74e21c494
martinfuchs <martinfuchs@f5dc347c-c33d-0410-90a0-b07cc1902cb9>
2007-11-10 20:15:48 +0000

    Added the check-svn-properties.sh script to the repository.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4736 f5dc347c-c33d-0410-90a0-b07cc1902cb9

---
+++ check-svn-properties.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#
+# check-svn-properties.sh
+#
+# This script sets the required Subversion properties for all files in
+# the current directory and all subdirectories based on file extensions.
+# As result the current difference to be checkig in is written to stdout.
+#
+
+svn up
+
+find . -name \*.php -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.php -exec svn propset svn:keywords 'Id' {} \; >/dev/null
+find . -name \*.txt -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.txt -exec svn propset svn:keywords 'Id' {} \; >/dev/null
+find . -name \*.css -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.js -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.sample -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.htm -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.html -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name .htaccess -exec svn propset svn:eol-style native {} \; >/dev/null
+find . -name \*.png -exec svn propset svn:mime-type image/png {} \; >/dev/null
+find . -name \*.jpg -exec svn propset svn:mime-type image/jpeg {} \; >/dev/null
+find . -name \*.gif -exec svn propset svn:mime-type image/gif {} \; >/dev/null
+
+svn di

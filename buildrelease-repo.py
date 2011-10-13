504f08ae184125bffb3b8eac86a66bed66d5f79e
John Reese <john@noswap.com>
2011-10-13 11:22:17 -0400

    Convert to four-space tabs

--- buildrelease-repo.py
+++ buildrelease-repo.py
@@ -13,16 +13,16 @@ buildscript = path.dirname(path.abspath(__file__)) + '/buildrelease.py'
 
 # Regular expressions of refs to ignore
 ignorelist = map(re.compile, [
-			'HEAD',
-			'-1\.0\.[\w\d]+',
-			])
+            'HEAD',
+            '-1\.0\.[\w\d]+',
+            ])
 
 # Script options
 options = "hfr:bacds:"
 long_options = [ "help", "fresh", "ref=", "branches", "auto-suffix", "clean", "docbook", "suffix=" ]
 
 def usage():
-	print '''Usage: buildrelease-repo [options] /path/for/tarballs [/path/to/repo]
+    print '''Usage: buildrelease-repo [options] /path/for/tarballs [/path/to/repo]
 Options:  -h | --help                  Show this usage message
 
           -f | --fresh                 Create a fresh clone at repository path, or temporary path
@@ -36,129 +36,129 @@ Options:  -h | --help                  Show this usage message
 #end usage()
 
 def ignore( ref ):
-	'''Decide which refs to ignore based on regexen listed in 'ignorelist'.
-	'''
+    '''Decide which refs to ignore based on regexen listed in 'ignorelist'.
+    '''
 
-	ignore = False
-	for regex in ignorelist:
-		if len(regex.findall(ref)) > 0:
-			ignore = True
-	return ignore
+    ignore = False
+    for regex in ignorelist:
+        if len(regex.findall(ref)) > 0:
+            ignore = True
+    return ignore
 #end ignore()
 
 def main():
-	try:
-		opts, args = getopt.gnu_getopt(sys.argv[1:], options, long_options)
-	except getopt.GetoptError, err:
-		print str(err)
-		usage()
-		sys.exit(2)
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], options, long_options)
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
 
-	pass_opts = ""
-	refs = []
-	all_branches = False
-	version_suffix = ""
-	auto_suffix = False
-	fresh_clone = False
-	delete_clone = False
+    pass_opts = ""
+    refs = []
+    all_branches = False
+    version_suffix = ""
+    auto_suffix = False
+    fresh_clone = False
+    delete_clone = False
 
-	for opt, val in opts:
-		if opt in ("-h", "--help"):
-			usage()
-			sys.exit(0)
+    for opt, val in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit(0)
 
-		elif opt in ("-f", "--fresh"):
-			fresh_clone = True
+        elif opt in ("-f", "--fresh"):
+            fresh_clone = True
 
-		elif opt in ("-r", "--ref"):
-			refs.extend(val.split(","))
+        elif opt in ("-r", "--ref"):
+            refs.extend(val.split(","))
 
-		elif opt in ("-b", "--branches"):
-			all_branches = True
+        elif opt in ("-b", "--branches"):
+            all_branches = True
 
-		elif opt in ("-c", "--clean"):
-			pass_opts += " -c"
+        elif opt in ("-c", "--clean"):
+            pass_opts += " -c"
 
-		elif opt in ("-d", "--docbook"):
-			pass_opts += " -d"
+        elif opt in ("-d", "--docbook"):
+            pass_opts += " -d"
 
-		elif opt in ("-a", "--auto-suffix"):
-			auto_suffix = True
+        elif opt in ("-a", "--auto-suffix"):
+            auto_suffix = True
 
-		elif opt in ("-s", "--suffix"):
-			version_suffix = val
+        elif opt in ("-s", "--suffix"):
+            version_suffix = val
 
-	if len(args) < 1:
-		usage()
-		sys.exit(1)
+    if len(args) < 1:
+        usage()
+        sys.exit(1)
 
-	release_path = args[0]
-	repo_path = "."
+    release_path = args[0]
+    repo_path = "."
 
-	if len(args) > 1:
-		repo_path = args[1]
+    if len(args) > 1:
+        repo_path = args[1]
 
-	# Create a new repo clone
-	if fresh_clone:
-		if repo_path == ".":
-			repo_path = tempfile.mkdtemp(prefix="mantisbt-", suffix=".git")
-			delete_clone = True
-		os.system('git clone git://github.com/mantisbt/mantisbt.git %s' % (repo_path))
+    # Create a new repo clone
+    if fresh_clone:
+        if repo_path == ".":
+            repo_path = tempfile.mkdtemp(prefix="mantisbt-", suffix=".git")
+            delete_clone = True
+        os.system('git clone git://github.com/mantisbt/mantisbt.git %s' % (repo_path))
 
-	# Change to the repo path
-	os.chdir(repo_path)
+    # Change to the repo path
+    os.chdir(repo_path)
 
-	# Update the repository
-	os.system('git fetch')
+    # Update the repository
+    os.system('git fetch')
 
-	# Consolidate refs/branches
-	if all_branches:
-		os.system('git remote prune origin')
-		refs.extend(os.popen('git branch -r').read().split())
+    # Consolidate refs/branches
+    if all_branches:
+        os.system('git remote prune origin')
+        refs.extend(os.popen('git branch -r').read().split())
 
-	if len(refs) < 1:
-		refs.extend(os.popen('git log --pretty="format:%h" -n1').read())
+    if len(refs) < 1:
+        refs.extend(os.popen('git log --pretty="format:%h" -n1').read())
 
-	refs = [ref for ref in refs if not ignore(ref)]
+    refs = [ref for ref in refs if not ignore(ref)]
 
-	# Info
-	print "Will build the following releases:"
-	for ref in refs:
-		print "  %s"%ref
-	print "\n"
+    # Info
+    print "Will build the following releases:"
+    for ref in refs:
+        print "  %s"%ref
+    print "\n"
 
-	# Regex to strip 'origin/' from ref names
-	refnameregex = re.compile('(?:[a-zA-Z0-9-.]+/)?(.*)')
-	
-	for ref in refs:
-		os.system("git checkout -f %s"%(ref))
+    # Regex to strip 'origin/' from ref names
+    refnameregex = re.compile('(?:[a-zA-Z0-9-.]+/)?(.*)')
 
-		# Handle suffix/auto-suffix generation
-		hash = os.popen('git log --pretty="format:%h" -n1').read()
-		if hash != ref:
-			ref = refnameregex.search( ref ).group(1)
-			hash = "%s-%s"%(ref,hash)
+    for ref in refs:
+        os.system("git checkout -f %s"%(ref))
 
-		suffix = ""
-		if auto_suffix and version_suffix:
-			suffix = "--suffix %s-%s"%(version_suffix, hash)
-		elif auto_suffix:
-			suffix = "--suffix %s"%hash
-		elif version_suffix:
-			suffix = "--suffix %s"%version_suffix
+        # Handle suffix/auto-suffix generation
+        hash = os.popen('git log --pretty="format:%h" -n1').read()
+        if hash != ref:
+            ref = refnameregex.search( ref ).group(1)
+            hash = "%s-%s"%(ref,hash)
 
-		# Start building
-		os.system("%s %s %s %s %s"%(buildscript, pass_opts, suffix, release_path, repo_path))
+        suffix = ""
+        if auto_suffix and version_suffix:
+            suffix = "--suffix %s-%s"%(version_suffix, hash)
+        elif auto_suffix:
+            suffix = "--suffix %s"%hash
+        elif version_suffix:
+            suffix = "--suffix %s"%version_suffix
 
-	# Cleanup temporary repo if needed
-	if delete_clone:
-		print "\nRemoving temporary clone."
-		shutil.rmtree(repo_path)
+        # Start building
+        os.system("%s %s %s %s %s"%(buildscript, pass_opts, suffix, release_path, repo_path))
 
-	# Done
-	print "\nAll builds completed."
+    # Cleanup temporary repo if needed
+    if delete_clone:
+        print "\nRemoving temporary clone."
+        shutil.rmtree(repo_path)
+
+    # Done
+    print "\nAll builds completed."
 
 #end main()
 
 if __name__ == "__main__":
-	main()
+    main()
--- buildrelease.py
+++ buildrelease.py
@@ -14,7 +14,7 @@ long_options = [ "help", "clean", "docbook", "version=", "suffix=" ]
 manualscript = path.dirname(path.abspath(__file__)) + '/docbook-manual.py'
 
 def usage():
-	print '''Usage: buildrelease [options] /path/for/tarballs [/path/to/mantisbt]
+    print '''Usage: buildrelease [options] /path/for/tarballs [/path/to/mantisbt]
 Options:  -h | --help               Show this usage message
           -c | --clean              Remove build directory when completed
           -d | --docbook            Build the docbook manuals
@@ -23,180 +23,180 @@ Options:  -h | --help               Show this usage message
 #end usage()
 
 def main():
-	try:
-		opts, args = getopt.gnu_getopt(sys.argv[1:], options, long_options)
-	except getopt.GetoptError, err:
-		print str(err)
-		usage()
-		sys.exit(2)
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], options, long_options)
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
 
-	build_docbook = False
-	clean_build = False
-	mantis_version = ""
-	version_suffix = ""
+    build_docbook = False
+    clean_build = False
+    mantis_version = ""
+    version_suffix = ""
 
-	for opt, val in opts:
-		if opt in ("-h", "--help"):
-			usage()
-			sys.exit(0)
+    for opt, val in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit(0)
 
-		elif opt in ("-c", "--clean"):
-			clean_build = True
+        elif opt in ("-c", "--clean"):
+            clean_build = True
 
-		elif opt in ("-d", "--docbook"):
-			build_docbook = True
+        elif opt in ("-d", "--docbook"):
+            build_docbook = True
 
-		elif opt in ("-v", "--version"):
-			mantis_version = val
+        elif opt in ("-v", "--version"):
+            mantis_version = val
 
-		elif opt in ("-s", "--suffix"):
-			version_suffix = val
+        elif opt in ("-s", "--suffix"):
+            version_suffix = val
 
-	if len(args) < 1:
-		usage()
-		sys.exit(1)
+    if len(args) < 1:
+        usage()
+        sys.exit(1)
 
-	release_path = args[0]
-	mantis_path = "."
+    release_path = args[0]
+    mantis_path = "."
 
-	if len(args) > 1:
-		mantis_path = args[1]
+    if len(args) > 1:
+        mantis_path = args[1]
 
-	# 'Standard' umask
-	old_umask = os.umask( 0002 )
+    # 'Standard' umask
+    old_umask = os.umask( 0002 )
 
-	# Check paths
-	if not path.isdir(release_path):
-		print "Creating release path..."
-		os.mkdir(release_path)
+    # Check paths
+    if not path.isdir(release_path):
+        print "Creating release path..."
+        os.mkdir(release_path)
 
-	if not path.isdir(mantis_path):
-		print "Error: mantis path is not a directory or does not exist."
-		sys.exit(3)
+    if not path.isdir(mantis_path):
+        print "Error: mantis path is not a directory or does not exist."
+        sys.exit(3)
 
-	if not path.isfile(path.join(mantis_path, "core.php"))\
-		or not path.isdir(path.join(mantis_path, "core"))\
-		or not path.isfile(path.join(mantis_path, "core", "constant_inc.php")):
+    if not path.isfile(path.join(mantis_path, "core.php"))\
+        or not path.isdir(path.join(mantis_path, "core"))\
+        or not path.isfile(path.join(mantis_path, "core", "constant_inc.php")):
 
-		print "Error: mantis path does not appear to be a valid Mantis directory."
-		sys.exit(3)
+        print "Error: mantis path does not appear to be a valid Mantis directory."
+        sys.exit(3)
 
-	# Find Mantis version
-	if not mantis_version:
-		f = open(path.join(mantis_path, "core", "constant_inc.php"))
-		content = f.read()
-		f.close
+    # Find Mantis version
+    if not mantis_version:
+        f = open(path.join(mantis_path, "core", "constant_inc.php"))
+        content = f.read()
+        f.close
 
-		mantis_version = re.search("'MANTIS_VERSION'[,\s]+'([^']+)'", content).group(1)
+        mantis_version = re.search("'MANTIS_VERSION'[,\s]+'([^']+)'", content).group(1)
 
-	# Generate release name
-	release_name = 'mantisbt-' + mantis_version
+    # Generate release name
+    release_name = 'mantisbt-' + mantis_version
 
-	if version_suffix:
-		release_name += '-' + version_suffix
+    if version_suffix:
+        release_name += '-' + version_suffix
 
-	# Copy to release path, removing custom files
-	release_dir = path.join(release_path, release_name)
+    # Copy to release path, removing custom files
+    release_dir = path.join(release_path, release_name)
 
-	print "Building release '%s' in path '%s'"%(release_name, release_dir)
+    print "Building release '%s' in path '%s'"%(release_name, release_dir)
 
-	if path.exists(release_dir):
-		print "Error: release path already contains %s."%(release_name)
-		sys.exit(3)
+    if path.exists(release_dir):
+        print "Error: release path already contains %s."%(release_name)
+        sys.exit(3)
 
-	os.system("rsync -rltD --exclude=.git %s/ %s"%(mantis_path, release_dir))
+    os.system("rsync -rltD --exclude=.git %s/ %s"%(mantis_path, release_dir))
 
-	# Apply version suffix
-	if version_suffix:
-		print "Applying version suffix..."
-		os.system("sed -r -i=bak \"s,g_version_suffix(\s+)=(\s*)'.*',g_version_suffix\\1=\\2'%s'\", %s"%(
-			version_suffix, path.join(release_dir, "config_defaults_inc.php")))
+    # Apply version suffix
+    if version_suffix:
+        print "Applying version suffix..."
+        os.system("sed -r -i=bak \"s,g_version_suffix(\s+)=(\s*)'.*',g_version_suffix\\1=\\2'%s'\", %s"%(
+            version_suffix, path.join(release_dir, "config_defaults_inc.php")))
 
-	# Walk through and remove custom files
-	print "Removing custom files from release path..."
+    # Walk through and remove custom files
+    print "Removing custom files from release path..."
 
-	def custom_files(name):
-		if name in (
-				".gitignore",
-				"config_inc.php",
-				"custom_constant_inc.php",
-				"custom_strings_inc.php",
-				"custom_functions_inc.php",
-				"mantis_offline.php",
-				"web.config"
-				):
-			return True
-		else:
-			return False
+    def custom_files(name):
+        if name in (
+                ".gitignore",
+                "config_inc.php",
+                "custom_constant_inc.php",
+                "custom_strings_inc.php",
+                "custom_functions_inc.php",
+                "mantis_offline.php",
+                "web.config"
+                ):
+            return True
+        else:
+            return False
 
-	def custom_dirs(name):
-		if name in (
-				"packages"
-				):
-			return True
-		else:
-			return False
+    def custom_dirs(name):
+        if name in (
+                "packages"
+                ):
+            return True
+        else:
+            return False
 
-	for root, dirs, files in os.walk(release_dir, topdown=False):
-		files = filter(custom_files, files)
-		dirs = filter(custom_dirs, dirs)
-		for name in files:
-			print "  " + path.join(root,name)
-			os.remove(path.join(root, name))
-		for name in dirs:
-			print "  " + path.join(root,name) + "/"
-			os.system("rm -rf %s"%path.join(root, name))
+    for root, dirs, files in os.walk(release_dir, topdown=False):
+        files = filter(custom_files, files)
+        dirs = filter(custom_dirs, dirs)
+        for name in files:
+            print "  " + path.join(root,name)
+            os.remove(path.join(root, name))
+        for name in dirs:
+            print "  " + path.join(root,name) + "/"
+            os.system("rm -rf %s"%path.join(root, name))
 
-	# Build documentation for release	
-	if build_docbook:
-		print "Building docbook manuals..."
-		os.system("%s --release %s %s"%(manualscript, path.join(release_dir, "docbook"), path.join(release_dir, "doc")))
+    # Build documentation for release
+    if build_docbook:
+        print "Building docbook manuals..."
+        os.system("%s --release %s %s"%(manualscript, path.join(release_dir, "docbook"), path.join(release_dir, "doc")))
 
-	# Create tarballs
-	print "Creating release tarballs..."
+    # Create tarballs
+    print "Creating release tarballs..."
 
-	os.chdir(release_path)
-	os.system("tar czf %s.tar.gz %s"%(release_name, release_name))
-	os.system("zip -rq %s.zip %s"%(release_name, release_name))
+    os.chdir(release_path)
+    os.system("tar czf %s.tar.gz %s"%(release_name, release_name))
+    os.system("zip -rq %s.zip %s"%(release_name, release_name))
 
-	# Generate checksums
-	print "Generating checksums..."
+    # Generate checksums
+    print "Generating checksums..."
 
-	tar_md5 = os.popen("md5sum --binary %s.tar.gz"%(release_name)).read()
-	tar_sha1 = os.popen("sha1sum --binary %s.tar.gz"%(release_name)).read()
+    tar_md5 = os.popen("md5sum --binary %s.tar.gz"%(release_name)).read()
+    tar_sha1 = os.popen("sha1sum --binary %s.tar.gz"%(release_name)).read()
 
-	zip_md5 = os.popen("md5sum --binary %s.zip"%(release_name)).read()
-	zip_sha1 = os.popen("sha1sum --binary %s.zip"%(release_name)).read()
+    zip_md5 = os.popen("md5sum --binary %s.zip"%(release_name)).read()
+    zip_sha1 = os.popen("sha1sum --binary %s.zip"%(release_name)).read()
 
-	f = open("%s.tar.gz.digests"%release_name, 'w')
-	f.write("%s\n%s\n"%(tar_md5, tar_sha1))
-	f.close()
+    f = open("%s.tar.gz.digests"%release_name, 'w')
+    f.write("%s\n%s\n"%(tar_md5, tar_sha1))
+    f.close()
 
-	f = open("%s.zip.digests"%release_name, 'w')
-	f.write("%s\n%s\n"%(zip_md5, zip_sha1))
-	f.close()
+    f = open("%s.zip.digests"%release_name, 'w')
+    f.write("%s\n%s\n"%(zip_md5, zip_sha1))
+    f.close()
 
-	print "  " + tar_md5
-	print "  " + tar_sha1
-	print "  " + zip_md5
-	print "  " + zip_sha1
+    print "  " + tar_md5
+    print "  " + tar_sha1
+    print "  " + zip_md5
+    print "  " + zip_sha1
 
-	# Cleanup
-	if clean_build:
-		print "Removing build directory..."
-		for root, dirs, files in os.walk(release_dir, topdown=False):
-			for name in files:
-				os.remove(path.join(root, name))
-			for name in dirs:
-				os.rmdir(path.join(root, name))
-		os.rmdir(release_dir)
+    # Cleanup
+    if clean_build:
+        print "Removing build directory..."
+        for root, dirs, files in os.walk(release_dir, topdown=False):
+            for name in files:
+                os.remove(path.join(root, name))
+            for name in dirs:
+                os.rmdir(path.join(root, name))
+        os.rmdir(release_dir)
 
-	# Restore previous umask
-	os.umask( old_umask )
-	
-	print "Done!"
+    # Restore previous umask
+    os.umask( old_umask )
+
+    print "Done!"
 
 #end main()
 
 if __name__ == "__main__":
-	main()
+    main()

c6ab8a23914ddbc938e168712b8ba204e58508d2
Damien Regad <dregad@mantisbt.org>
2017-01-27 01:50:07 +0100

    Config module for python scripts
    Implement a simple config module for Python scripts based on YAML files.
    
    Default configuration is stored in `config_defaults.yml`. This file also
    documents the available settings, which can be overriden as needed from
    a `config.yml` file.

---
+++ config.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python -u
+"""
+Configuration for various MantisBT utility scripts
+"""
+
+import yaml
+import collections
+
+
+# Constants
+ORG_MANTIS = 'mantisbt'
+ORG_PLUGINS = 'mantisbt-plugins'
+
+
+class Config(dict):
+    """
+    The Config class extends dict wrapper allowing its keys to be accessed as
+    attributes for convenience.
+    """
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        raise AttributeError(name)
+
+
+def __update(base, upd):
+    """
+    Recursively update 'base' dict with corresponding values in 'upd'
+    """
+    for key, value in upd.iteritems():
+        if isinstance(value, collections.Mapping):
+            base[key] = __update(base.get(key, {}), value)
+        else:
+            base[key] = upd[key]
+    return base
+
+
+def __read_config():
+    """
+    Read YML config files and return the data
+    """
+
+    # Read default configuration values
+    with open("config_defaults.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+    # Read local settings
+    with open("config.yml", 'r') as ymlfile:
+        cfg = __update(cfg, yaml.load(ymlfile))
+
+    return Config(cfg)
+
+
+cfg = __read_config()

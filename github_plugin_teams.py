5ffa2af872900d37f8a859ff9792d80dd31afb9f
Damien Regad <dregad@mantisbt.org>
2019-08-06 15:45:12 +0200

    Update Github plugins script to Python 3

--- github_plugin_teams.py
+++ github_plugin_teams.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/python3 -u
 """
 Uses Github API to grant write access to all of mantisbt-plugins organization
 to the teams specified in config file
@@ -37,7 +37,7 @@ def retrieve_teams(org):
     # Check for invalid team names in config file
     invalid = set(cfg.github['teams']) - set(teams)
     if invalid:
-        print "Unknown teams specified in configuration:", ', '.join(invalid)
+        print("Unknown teams specified in configuration:", ', '.join(invalid))
         sys.exit(1)
 
     return teams
@@ -55,54 +55,54 @@ def retrieve_team_repos(team):
 
 
 def main():
-    print "Connecting to Github as '{0}'".format(cfg.github['user'])
+    print("Connecting to Github as '{0}'".format(cfg.github['user']))
     g = Github(cfg.github['user'], cfg.github['password'])
 
     # Organization
-    print "Retrieving organization '{0}'".format(config.ORG_PLUGINS)
+    print("Retrieving organization '{0}'".format(config.ORG_PLUGINS))
     org = g.get_organization(config.ORG_PLUGINS)
 
     # Teams
-    print "Retrieving teams...",
+    print("Retrieving teams...",)
     try:
         teams = retrieve_teams(org)
     except GithubException as err:
         print
         if err.status == 401:
-            print "This script requires authentication with a privileged " \
-                "account to access and update the organization's team."
-            print "Please update the configuration as appropriate"
+            print("This script requires authentication with a privileged "
+                  "account to access and update the organization's team.")
+            print("Please update the configuration as appropriate")
         else:
-            print "Unknown error", (err)
+            print("Unknown error", (err))
         sys.exit(1)
-    print len(teams), "found"
+    print(len(teams), "found")
 
     # Repositories
-    print "Retrieving the organization's repositories...",
+    print("Retrieving the organization's repositories...",)
     org_repos = retrieve_org_repos(org)
-    print len(org_repos), "found"
+    print(len(org_repos), "found")
 
     # Check that the team grants access to each of the org's repo
     # Add write access if not
     for team in teams.values():
-        print "Processing team '{0}'".format(team.name)
+        print("Processing team '{0}'".format(team.name))
         count = 0
         access = cfg.github['teams'][team.name]
 
-        print "  Retrieving repositories...",
+        print("  Retrieving repositories...",)
         team_repos = retrieve_team_repos(team)
-        print len(team_repos), "found"
+        print(len(team_repos), "found")
 
-        print "  Checking for missing access"
+        print("  Checking for missing access")
         for repo in org_repos:
             if repo.id not in team_repos:
                 count += 1
-                print "  Grant {0} access to plugin '{1}'".format(
+                print("  Grant {0} access to plugin '{1}'".format(
                     access,
                     repo.name
-                    )
+                    ))
                 team.set_repo_permission(repo, access)
-        print "  {0} plugins processed".format(count)
+        print("  {0} plugins processed".format(count))
 
 
 if __name__ == "__main__":

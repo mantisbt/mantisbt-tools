5af7f0fd62552124929ce06cc6881d3beeb51e97
Damien Regad <dregad@mantisbt.org>
2020-05-16 14:29:53 +0200

    Handle error if user/repo is not found
    Renamed function repo() -> get_repo().
    
    Function used to return just the repository's full name (user/repo),
    now it returns a repository object and exits if not found.

--- merged_pr_branches.py
+++ merged_pr_branches.py
@@ -16,9 +16,10 @@ that can be used to delete the no-longer needed branches.
 import os
 from os import path
 import subprocess
+import sys
 
 # Github module https://github.com/jacquev6/PyGithub
-from github import Github
+from github import Github, GithubException
 
 # Edit the variables below as appropriate
 github_token = None
@@ -27,31 +28,45 @@ author = ''             # GitHub ID of the PR's author
 repo_name = 'mantisbt'  # Repository name
 
 
-def repo(user, repo):
+def get_repo(user, repo):
     '''
-    Return GitHub query to search for all merged PRs submitted by given author
-    in target organization and repository.
+    Return a GitHub repository object
     '''
-    return '{0}/{1}'.format(user, repo)
+    fullname = '{0}/{1}'.format(user, repo)
+
+    try:
+        repo = gh.get_repo(fullname)
+    except GithubException as e:
+        print("ERROR: repository '{}' not found".format(fullname))
+        sys.exit(1)
+
+    return repo
 
 
 def main():
     print('Connecting to GitHub')
+    global gh
     gh = Github(github_token)
 
-    author_repo = repo(author, repo_name)
-    refs = gh.get_repo(author_repo).get_git_refs()
+    # Retrieve the list of branches in the author's repository
+    author_repo = get_repo(author, repo_name)
+    refs = author_repo.get_git_refs()
     branches = list(ref.ref.replace('refs/heads/', '') for ref in refs if ref.ref.startswith('refs/heads/'))
-    print '{} branches found in {}'.format(len(branches), author_repo)
+    print('{} branches found in {}'.format(len(branches),
+                                           author_repo.full_name))
 
-    target_repo = repo(target_org, repo_name)
+    # Retrieve the list of all merged PRs submitted by given author in the
+    # target organization's repository
+    target_repo = get_repo(target_org, repo_name)
     merged_pr = gh.search_issues(query='', **{
-        'repo':target_repo,
+        'repo':target_repo.full_name,
         'author':author,
         'type':'pr',
         'is':'merged'
         })
-    print '{} merged pull requests found in {}'.format(merged_pr.totalCount, target_repo)
+    print('{} merged pull requests found in {}'
+          .format(merged_pr.totalCount, target_repo.full_name))
+
     print 'Retrieving corresponding head branches (this may take a while)...'
     merged_branches = {}
     for pr in merged_pr:
@@ -67,7 +82,8 @@ def main():
             count += 1
             print '{},{}'.format(branch, merged_branches[branch])
     print
-    print '{} branches in {} have been merged in {}'.format(count, author_repo, target_repo)
+    print('{} branches in {} have been merged in {}'
+          .format(count, author_repo.full_name, target_repo.full_name))
 
 if __name__ == "__main__":
     main()

b46ed8f38f5f8fd0f0b368704a7172361fc6b3e5
Damien Regad <dregad@mantisbt.org>
2020-05-17 11:24:29 +0200

    Python 3

--- get_all_repos.py
+++ get_all_repos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/python3 -u
 """
 Clones or updates local copies of all the specified organization's repos
 
@@ -7,7 +7,7 @@ organization's repos.
 """
 
 import os
-from os import path
+from pathlib import Path
 import subprocess
 
 # Github module https://github.com/jacquev6/PyGithub
@@ -21,35 +21,36 @@ github_org = config.ORG_PLUGINS
 
 
 def main():
-    print "Retrieving all plugins from '%s' organization" % github_org
+    print("Retrieving all plugins from '{}' organization".format(github_org))
 
-    print "Connecting to Github"
+    print("Connecting to Github")
     gh = Github(cfg.github['token'])
     org = gh.get_organization(github_org)
 
     # Process all repos
     for repo in org.get_repos():
-        print "\nProcessing plugin '%s'" % repo.name
+        print("\nProcessing plugin '{}'".format(repo.name))
 
         # Local repo exists, change to default branch
-        if path.isdir(repo.name):
-            print "  Updating local repository"
+        if Path(repo.name).is_dir():
+            print("  Updating local repository")
             os.chdir(repo.name)
-            print "    Checkout default branch '%s'" % repo.default_branch
-            subprocess.check_call('git checkout %s' % repo.default_branch,
+            print("    Checkout default branch '{}'"
+                  .format(repo.default_branch))
+            subprocess.check_call('git checkout ' + repo.default_branch,
                                   shell=True)
-            print "    Pulling from remote"
+            print("    Pulling from remote")
             subprocess.check_call('git pull', shell=True)
 
         # Cloning repo if not found locally
         else:
-            print "  Local repo not found, cloning..."
-            subprocess.check_call('git clone %s' % repo.clone_url, shell=True)
+            print("  Local repo not found, cloning...")
+            subprocess.check_call('git clone ' + repo.clone_url, shell=True)
             os.chdir(repo.name)
 
         os.chdir('..')
 
-    print "\nDone"
+    print("\nDone")
 
 
 if __name__ == "__main__":

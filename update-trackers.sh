mantisbt/mantisbt-tools

commit 4658d62db2a8344c39307aafd9b8e47bab96bc96

Fix shellcheck warnings

update-trackers.sh
@@ -37,7 +37,7 @@ UPDATE_BRANCH=master
 
 function usage() {
 	cat <<-EOF
-		Syntax: $(basename $0) [-b Branch] [-c Commit] repo [...]
+		Syntax: $(basename "$0") [-b Branch] [-c Commit] repo [...]
 
 		Branch	Reference branch (defaults to '$UPDATE_BRANCH')
 		Commit	Target commit to rebase tracker's branch to
@@ -50,7 +50,7 @@ function process_error() {
 	echo -ne "\nERROR: "
 	if [ -n "$1" ]
 	then
-		echo $1
+		echo "$1"
 	fi
 
 	if [ -n "$REPO" ]
@@ -91,7 +91,7 @@ test -z "$TARGET_REF" &&
 	TARGET_REF=$UPDATE_BRANCH
 
 # Remaining arguments should be the list of trackers to process
-shift $((${OPTIND}-1))
+shift $((OPTIND - 1))
 if [ -z "$1" ]
 then
 	usage
@@ -104,27 +104,27 @@ fi
 # Main
 #
 
-for REPO in $*
+for REPO in "$@"
 do
 	echo -e "Processing target repository: $REPO\n"
 
-	DIR_NAME=$(dirname $REPO)
+	DIR_NAME=$(dirname "$REPO")
 	if [[ -z "$DIR_NAME" || $DIR_NAME == "." ]]
 	then
 		DIR_NAME=$PWD
 	fi
-	REPO=$(basename $REPO)
+	REPO=$(basename "$REPO")
 
-	cd $DIR_NAME/$REPO 2>/dev/null ||
+	cd "$DIR_NAME/$REPO" 2>/dev/null ||
 		process_error "repository '$REPO' does not exist in '$DIR_NAME' or is not accessible"
 
 	# Detect if there are unstaged changes in the repository's current branch
-	git diff-index --name-status --exit-code HEAD
-	if [ $? -ne 0 ]
+	if git diff-index --name-status --exit-code HEAD
 	then
 		echo -e "\nThere are unstaged changes"
-		read -n 1 -p "Would you like to discard them ? "
+		read -r -n 1 -p "Would you like to discard them ? "
 		echo
+		# shellcheck disable=SC2086
 		if [ "$(echo ${REPLY} | tr "[:upper:]" "[:lower:]")" = "y" ]
 		then
 			echo "Discarding changes"
@@ -136,7 +136,7 @@ do
 
 	# First update the reference branch, pull changes from upstream
 	echo "- Pulling upstream changes into reference branch '$UPDATE_BRANCH'"
-	git checkout $UPDATE_BRANCH ||
+	git checkout "$UPDATE_BRANCH" ||
 		process_error "Unable to checkout branch '$UPDATE_BRANCH' !"
 	git pull --ff-only ||
 		process_error "failed to fast-forward branch '$UPDATE_BRANCH'"
@@ -144,16 +144,15 @@ do
 	# Set the version suffix as 'RefBranch-CommitSHA'
 	# If we're on a release tag, then the suffix is blank
 	unset VERSION_SUFFIX
-	git describe $TARGET_REF --exact-match >/dev/null 2>&1 ||
-		VERSION_SUFFIX=-$UPDATE_BRANCH-$(git rev-parse --short $TARGET_REF)
+	git describe "$TARGET_REF" --exact-match >/dev/null 2>&1 ||
+		VERSION_SUFFIX=-$UPDATE_BRANCH-$(git rev-parse --short "$TARGET_REF")
 
 	# If the repository-specific branch exists, we rebase it on the top of the
 	# reference branch
-	git checkout $REPO 2>/dev/null
-	if [ $? -eq 0 ]
+	if git checkout "$REPO" 2>/dev/null
 	then
 		echo "- Rebasing local branch '$REPO' to '$TARGET_REF'"
-		git rebase $TARGET_REF ||
+		git rebase "$TARGET_REF" ||
 			process_error "failed to rebase '$REPO' branch to '$TARGET_REF'"
 	else
 		echo "- WARNING: Unable to checkout local branch '$REPO'"
@@ -167,8 +166,7 @@ do
 	# Updating the version suffix in config file
 	echo "- Setting version_suffix to '$VERSION_SUFFIX' in $CONFIG_FILE"
 	test -f $CONFIG_FILE || process_error "missing $CONFIG_FILE"
-	grep "^\s*\$g_version_suffix" $CONFIG_FILE >/dev/null
-	if [ $? -eq 0 ]
+	if grep "^\s*\$g_version_suffix" $CONFIG_FILE >/dev/null
 	then
 		echo "Updating existing config file"
 		sed -r -i.bak "s/^(\s*\\\$g_version_suffix\s*=\s*[\"']).*([\"'])/\1$VERSION_SUFFIX\2/" $CONFIG_FILE
@@ -183,16 +181,18 @@ do
 
 	# Cleanup
 	echo
-	read -n 1 -p "The 'admin' directory should be deleted. Would you like to do it now ? "
+	read -r -n 1 -p "The 'admin' directory should be deleted. Would you like to do it now ? "
 	echo
 	ADMIN_DIR=$PWD/admin
+  # shellcheck disable=SC2086
 	if [ "$(echo ${REPLY} | tr "[:upper:]" "[:lower:]")" = "y" ]
 	then
-		rm -rvf $ADMIN_DIR
+		rm -rvf "$ADMIN_DIR"
 	else
 		echo "WARNING: Remember to delete it after completing the upgrade (rm -rf $ADMIN_DIR)"
 	fi
 
 	echo -e "\nRepository '$REPO' updated successfully\n"
+	# shellcheck disable=SC2164
 	cd - >/dev/null
 done

commit 551846f781f367381b9ba4b489152c5bf8be48ed

Add missing `!`
Regression from 4658d62db2a8344c39307aafd9b8e47bab96bc96

update-trackers.sh
@@ -119,7 +119,7 @@ do
 		process_error "repository '$REPO' does not exist in '$DIR_NAME' or is not accessible"
 
 	# Detect if there are unstaged changes in the repository's current branch
-	if git diff-index --name-status --exit-code HEAD
+	if ! git diff-index --name-status --exit-code HEAD
 	then
 		echo -e "\nThere are unstaged changes"
 		read -r -n 1 -p "Would you like to discard them ? "